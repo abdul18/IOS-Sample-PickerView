ba595dc0103c1e2936805ad1e144ef4bc1a3cf4f
Yosemite <yosemite@Yosemites-Mac-mini.local>
2015-10-20 12:12:54 +0530

    Initial Commit

---
+++ pickerViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickerViewExample
+//
+//  Created by Yosemite on 20/10/15.
+//  Copyright (c) 2015 Pavaratha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
